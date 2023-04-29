4bddfd50fc9f4adf081d937d0576518107b205c2
任帅 <1064425721@qq.com>
2023-04-29 11:26:52 +0800

    chore: 修改mouse事件监听

--- src/components/Draggable.ts
+++ src/components/Draggable.ts
@@ -1,35 +1,39 @@
 /**
  * @description 自定义拖拽
  * @param {HTMLElement} dragDom 被拖拽的区域
- * @param {HTMLElement} tagDom 移动的元素
+ * @param {HTMLElement} moveDom 移动的元素
  */
-export function customDraggable(dragDom: HTMLElement, tagDom: HTMLElement) {
+export function customDraggable(dragDom: HTMLElement, moveDom: HTMLElement) {
   let offsetX = 0;
   let offsetY = 0;
+  let moveX = 0;
+  let moveY = 0;
+  let x = 0;
+  let y = 0;
+  function mouseMove(e: MouseEvent) {
+    x = moveX+ e.clientX - offsetX;
+    y = moveY+ e.clientY - offsetY;
+    console.log(moveX, moveY)
+    console.log(x,y)
 
-  function mouseUp() {
-    console.log('鼠标离开了!')
-    dragDom.removeEventListener('mousemove', mouseMove);
+    moveDom.style.left = x + "px";
+    moveDom.style.top = y + "px";
   }
 
-  function mouseMove(e) {
-    let x = e.clientX - offsetX;
-    let y = e.clientY - offsetY;
-    tagDom.style.left = x + "px";
-    tagDom.style.top = y + "px";
-  }
-
-  function mouseDown(e) {
+  function mouseDown(e: MouseEvent) {
     offsetX = e.clientX - dragDom.offsetLeft || 0;
     offsetY = e.clientY - dragDom.offsetTop || 0;
-
-    dragDom.addEventListener('mousemove', (move) => {
-      mouseMove(move)
-    });
+    dragDom.addEventListener('mousemove', mouseMove);
   }
-
-  dragDom.addEventListener('mousedown', function (e) {
-    mouseDown(e)
+  dragDom.addEventListener('mousedown', mouseDown);
+  dragDom.addEventListener('mouseout', function () {
+    dragDom.removeEventListener('mousemove', mouseMove);
+    moveX = x;
+    moveY = y;
   });
-  dragDom.addEventListener('mouseup', mouseUp);
+  dragDom.addEventListener('mouseup', function () {
+    dragDom.removeEventListener('mousemove', mouseMove);
+    moveX = x;
+    moveY = y;
+  });
 }
--- src/components/Draggable.vue
+++ src/components/Draggable.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="dom2">
-  <div class="dom1"></div>
+  <div class="move">
+    <div class="darg"></div>
+    <div class="dom"></div>
   </div>
 </template>
 
@@ -8,28 +9,29 @@
 import { nextTick } from 'vue';
 import { customDraggable } from './Draggable'
 nextTick(() => {
-  const targetDom = document.querySelector('.dom1') as HTMLElement;
-  const dragDom = document.querySelector('.dom2') as HTMLElement;
-  console.log(targetDom)
-  console.log(dragDom)
-  customDraggable(targetDom, dragDom)
+  const dragDom = document.querySelector('.darg') as HTMLElement;
+  const moveDom = document.querySelector('.move') as HTMLElement;
+  customDraggable(dragDom, moveDom)
 })
 </script>
 
 <style scoped>
-
-.dom1 {
-  height: 50px;
-  width: 100px;
-  background: rgb(17, 216, 17);
-  margin: 20px;
-}
-
-.dom2 {
-   position: relative;
+.move {
+  position: relative;
   height: 50px;
   width: 100px;
   background: rgb(2, 1, 1);
-   margin: 20px;
+}
+
+.darg {
+  height: 50px;
+  width: 100px;
+  background: rgb(17, 216, 17);
+}
+
+.dom {
+  height: 50px;
+  width: 100px;
+  background: rgb(2, 1, 1);
 }
 </style>
